46b732bfbb6b218257b4f9e1986796ef205260cf
Michael <130604613+BassyCounter@users.noreply.github.com>
2024-04-27 22:59:24 -0500

    Create teamcolor.js

---
+++ scripts/teamcolor.js
@@ -0,0 +1,10 @@
+let main = document.querySelector('main');
+document.querySelector('#valor').addEventListener('click', function() {
+    main.style.backgroundColor = 'red';
+});
+document.querySelector('#instinct').addEventListener('click', function() {
+    main.style.backgroundColor = 'yellow';
+});
+document.querySelector('#mystic').addEventListener('click', function() {
+    main.style.backgroundColor = 'blue';
+});
